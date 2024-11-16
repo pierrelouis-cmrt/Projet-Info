84f440d85b2c9992e5818be6c5ad79f1afca8b79
Josh <113625417+pierrelouis-cmrt@users.noreply.github.com>
2024-11-16 08:39:54 +0000

    fixed termcolor color name

--- Version D.py
+++ Version D.py
@@ -592,7 +592,7 @@ def code_central():
                 colored("Le joueur ", 'red') + colored(str(joueur), 'blue') +
                 " n'a plus de cartes.")
         elif joueur == 1:
-            print("Vos cartes sont :", colored(str(dico[joueur]), 'orange'))
+            print("Vos cartes sont :", colored(str(dico[joueur]), 'light_red'))
 
         print("\n")
         points_gagnes, dico = action(dico, joueur)
